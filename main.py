ae10d1613ed583da8304be0844af05dce9dffb06
Tuan Nguyen <tuan.nguyen@tuni.fi>
2020-12-21 21:28:59 +0700

    Add UK stats and update README

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import world
 import vietnam
 import finland
+import uk
 
-choice = int(input("1. World\n2. Vietnam\n3. Finland\n\nYour choice: "))
+choice = int(input("1. World\n2. Vietnam\n3. Finland\n4. England\n\nYour choice: "))
 print()
 if choice == 1:
     world.world()
@@ -10,5 +11,7 @@ elif choice == 2:
     vietnam.vietnam()
 elif choice == 3:
     finland.finland()
+elif choice == 4:
+    uk.uk()
 else:
     print("Run the program again and choose from 1 to 3.")
